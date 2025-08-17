912c50b52927a16f0d7241d8d9379fbeea5a2bee
kibblewibble <jacksonbkilgore@gmail.com>
2025-08-17 16:17:24 -0400

    testing the webhook

--- src/main/java/net/verdantmods/insignis/Insignis.java
+++ src/main/java/net/verdantmods/insignis/Insignis.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//test
 public class Insignis implements ModInitializer {
 	public static final String MOD_ID = "insignis";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
