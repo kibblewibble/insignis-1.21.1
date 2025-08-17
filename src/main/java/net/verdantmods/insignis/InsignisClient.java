503b5ad1ba9969f79ed1768e7edcb144b12a1401
kibblewibble <jacksonbkilgore@gmail.com>
2025-08-17 16:10:49 -0400

    Initial commit

---
+++ src/main/java/net/verdantmods/insignis/InsignisClient.java
@@ -0,0 +1,10 @@
+package net.verdantmods.insignis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InsignisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/verdantmods/insignis/InsignisDataGenerator.java
@@ -0,0 +1,11 @@
+package net.verdantmods.insignis;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InsignisDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
