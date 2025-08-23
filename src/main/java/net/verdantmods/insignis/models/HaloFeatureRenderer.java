1d8e2755b96a38c723188b11dc4856e81bdfa936
kibblewibble <jacksonbkilgore@gmail.com>
2025-08-23 01:29:43 -0400

    lets hope this doesnt nuke it added a comment i halorendered

--- src/main/java/net/verdantmods/insignis/models/HaloFeatureRenderer.java
+++ src/main/java/net/verdantmods/insignis/models/HaloFeatureRenderer.java
@@ -15,6 +15,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.RotationAxis;
 import net.verdantmods.insignis.Insignis;
 
+import java.awt.*;
+
 
 public class HaloFeatureRenderer extends FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
     ModelPart halo;
@@ -28,11 +30,13 @@ public class HaloFeatureRenderer extends FeatureRenderer<AbstractClientPlayerEnt
     @Override
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(headYaw));
+        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-35));
         getContextModel().body.copyTransform(halo);
         VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityTranslucentCull(getTexture(entity)));
         int m = LivingEntityRenderer.getOverlay(entity, 0.0F);
         matrices.translate(0, entity.isSneaky() ? -.45F : -.75F, -.25);
         matrices.scale(1F, 0F, 1F);
+        halo.render(matrices, vertexConsumer, light, m, -200);
     }
 
     @Override
